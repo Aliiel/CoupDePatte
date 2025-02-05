ae042f6782f900be3edd3232577a9fc9d39fd31d
Ngandos <76482435+Ngandos@users.noreply.github.com>
2025-02-05 10:28:55 +0100

    AjoutAnnotations

--- src/main/java/org/mma/CoupDePatte/Models/Repositories/CityRepository.java
+++ src/main/java/org/mma/CoupDePatte/Models/Repositories/CityRepository.java
@@ -1,6 +1,8 @@
 package org.mma.CoupDePatte.Models.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CityRepository extends JpaRepository<City, Long> {
 }
