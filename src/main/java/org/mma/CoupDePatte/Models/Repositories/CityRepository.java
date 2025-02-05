e969d3c7ca8de1abf41bc0a7ba44ffd2a1b00d1c
Ngandos <76482435+Ngandos@users.noreply.github.com>
2025-02-05 09:09:54 +0100

    InitiationDesRepository

---
+++ src/main/java/org/mma/CoupDePatte/Models/Repositories/CityRepository.java
@@ -0,0 +1,6 @@
+package org.mma.CoupDePatte.Models.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CityRepository extends JpaRepository<City, Long> {
+}
