630e7110c6e219cabc8f700890c05f33c95d0ca4
Ngandos <76482435+Ngandos@users.noreply.github.com>
2025-02-05 12:28:28 +0100

    ControllerInitFinished

---
+++ src/main/java/org/mma/CoupDePatte/Controllers/UserController.java
@@ -0,0 +1,10 @@
+package org.mma.CoupDePatte.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@GetMapping("/users")
+public class UserController {
+
+}
