82c6623102bfbe95d701e67dc50e298608138f35
Isabelle Marand <isamarand27@gmail.com>
2025-02-05 13:23:40 +0100

    [IM] initialisation des tables et repository specie

---
+++ src/main/java/org/mma/CoupDePatte/Models/DTO/FilterDTO.java
@@ -0,0 +1,23 @@
+package org.mma.CoupDePatte.Models.DTO;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
+public record FilterDTO(
+        //Gestion des filtres choisis par l'utilisateur pour lui envoyer les annonces correspondant à son filtre
+        @NotNull(message = "Merci de préciser le type d'annonces cherché")
+        boolean isTrouve,
+        @NotBlank(message = "Précisez à proximité de quelle ville")
+        String town,
+        @NotNull (message = "Précisez à partir de quelle date faire la recherche")
+        LocalDate EventDate,
+        @NotBlank (message = "Sélectionnez une espèce d'animal")
+        String specie,
+        String breed,
+        String gender,
+        String eyes,
+        String coat,
+        boolean tattoo
+        ){}
